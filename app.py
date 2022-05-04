68bf3786cb47379927cdee65847011db94878b3e
carson <carson@leaddyno.com>
2022-05-04 13:29:45 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import (
+    Flask, redirect, render_template, request, flash
+)
+
+from contacts_model import Contact
+
+Contact.load_db()
+
+
+
+# ========================================================
+# Flask App
+# ========================================================
+
+app = Flask(__name__)
+
+app.secret_key = b'hypermedia rocks'
+
+@app.route("/")
+def index():
+    return redirect("/contacts")
+
+@app.route("/contacts")
+def contacts():
+    search = request.args.get("q")
+    if search:
+        contacts = Contact.search(search)
+    else:
+        contacts = Contact.all()
+    return render_template("index.html", contacts=contacts)
+
+
+@app.route("/contacts/new", methods=['POST', 'GET'])
+def contacts_new():
+    if request.method == 'POST':
+        c = Contact(None, request.form['first_name'], request.form['last_name'], request.form['phone'],
+                    request.form['email'])
+        c.save()
+        flash("Created New Contact!")
+        return redirect("/contacts")
+    else:
+        return render_template("new.html")
+
+
+@app.route("/contacts/<contact_id>")
+def contacts_view(contact_id=0):
+    contact = Contact.find(contact_id)
+    return render_template("show.html", contact=contact)
+
+
+@app.route("/contacts/<contact_id>/edit", methods=["POST", "GET"])
+def contacts_edit(contact_id=0):
+    contact = Contact.find(contact_id)
+    if request.method == 'POST':
+        contact.update(request.form['first_name'], request.form['last_name'], request.form['phone'],
+                       request.form['email'])
+        flash("Updated Contact!")
+        return redirect("/contacts/" + str(contact_id))
+    else:
+        return render_template("edit.html", contact=contact)
+
+
+@app.route("/contacts/<contact_id>/delete", methods=["POST"])
+def contacts_delete(contact_id=0):
+    contact = Contact.find(contact_id)
+    contact.delete()
+    flash("Deleted Contact!")
+    return redirect("/contacts")
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ contacts_model.py
@@ -0,0 +1,75 @@
+import json
+from operator import attrgetter
+
+#========================================================
+# Contact Model
+#========================================================
+class Contact:
+
+    # mock contacts database
+    db = []
+
+    def __init__(self, id, first, last, phone, email):
+        self.id = id
+        self.first = first
+        self.last = last
+        self.phone = phone
+        self.email = email
+
+    def __str__(self):
+        return json.dumps(dict(self), ensure_ascii=False)
+
+    def update(self, first, last, phone, email):
+        self.first = first
+        self.last = last
+        self.phone = phone
+        self.email = email
+        self.save()
+
+    def save(self):
+      if self.id is None:
+        max_id = max(Contact.db, key=lambda contact: contact.id)
+        self.id = max_id.id + 1
+        Contact.db.append(self)
+      Contact.save_db()
+
+    def delete(self):
+        Contact.db.remove(self)
+        Contact.save_db()
+
+    @staticmethod
+    def all():
+        return Contact.db
+
+    @staticmethod
+    def search(str):
+      result = []
+      for c in Contact.db:
+        if str in c.first or str in c.last or str in c.email or str in c.phone:
+           result.append(c)
+      return result
+
+    @staticmethod
+    def load_db():
+        with open('contacts.json', 'r') as contacts_file:
+          contacts = json.loads(contacts_file.read())
+          Contact.db.clear()
+          for c in contacts:
+            Contact.db.append(Contact(c['id'], c['first'], c['last'], c['phone'], c['email']))
+
+    @staticmethod
+    def save_db():
+        out_arr = []
+        for c in Contact.db:
+          out_arr.append(c.__dict__)
+        print(out_arr)
+        json_str = json.dumps(out_arr)
+        f = open("contacts.json", "w")
+        f.write(json_str)
+        f.close()
+
+    @staticmethod
+    def find(id):
+      id = int(id)
+      c = next(filter(lambda c: c.id == id, Contact.db), None)
+      return c
