049847ced60fcc96a653cb190b6e78576b870938
Deniz Akşimşek <deniz@denizaksimsek.com>
2022-10-11 22:51:05 +0300

    update rsjs menu

--- static/js/rsjs-menu.js
+++ static/js/rsjs-menu.js
@@ -1,73 +1,68 @@
-import { nanoid } from "https://esm.sh/nanoid@4.0.0/non-secure/index.js";
+// @ts-nocheck
 
-document.querySelectorAll("[data-menu]").forEach(menu => {
-  const
-  button = menu.querySelector("[data-menu-button]"),
-  body = menu.querySelector("[data-menu-items]"),
-  items = Array.from(body.querySelectorAll("[data-menu-item]"));
+function overflowMenu(subtree = document) {
+  subtree.querySelectorAll("[data-overflow-menu]").forEach(menuRoot => {
+    const
+    button = menuRoot.querySelector("[aria-haspopup]"),
+    menu = menuRoot.querySelector("[role=menu]"),
+    items = [...menu.querySelectorAll("[role=menuitem]")];
 
-  const isOpen = () => menu.dataset.menu === "open";
+    const isOpen = () => !menu.hidden;
 
-  const bodyId = body.id ?? (body.id = nanoid());
+    items.forEach(item => item.setAttribute("tabindex", "-1"));
 
-  button.setAttribute("aria-haspopup", "menu");
-  button.setAttribute("aria-controls", bodyId);
-
-  body.setAttribute("role", "menu");
-
-  items.forEach(item => {
-    item.setAttribute("role", "menuitem");
-    item.setAttribute("tabindex", "-1");
-  });
-
-
-  function toggleMenu(open = !isOpen()) {
-    if (open) {
-      menu.dataset.menu = "open";
-      body.hidden = false;
-      button.setAttribute("aria-expanded", "true");
-      items[0].focus();
-    } else {
-      menu.dataset.menu = "closed";
-      body.hidden = true;
-      button.setAttribute("aria-expanded", "false");
+    function toggleMenu(open = !isOpen()) {
+      if (open) {
+        menu.hidden = false;
+        button.setAttribute("aria-expanded", "true");
+        items[0].focus();
+      } else {
+        menu.hidden = true;
+        button.setAttribute("aria-expanded", "false");
+      }
     }
-  }
 
-  toggleMenu(isOpen());
-  button.addEventListener("click", () => toggleMenu());
+    toggleMenu(isOpen());
+    button.addEventListener("click", () => toggleMenu());
+    menuRoot.addEventListener("blur", e => console.log(e) || toggleMenu(false));
 
-  window.addEventListener("click", function clickAway(event) {
-    if (!menu.isConnected) window.removeEventListener("click", clickAway);
-    if (menu.contains(event.target)) return;
-    toggleMenu(false);
-  })
+    window.addEventListener("click", function clickAway(event) {
+      if (!menuRoot.isConnected) window.removeEventListener("click", clickAway);
+      if (!menuRoot.contains(event.target)) toggleMenu(false);
+    })
 
-  const currentIndex = () => {
-    const idx = items.indexOf(document.activeElement);
-    if (idx === -1) return 0;
-    return idx;
-  }
-
-  menu.addEventListener("keydown", e => {
-    if (e.key === "ArrowUp") {
-      items[currentIndex() - 1]?.focus();
-
-    } else if (e.key === "ArrowDown") {
-      items[currentIndex() + 1]?.focus();
-
-    } else if (e.key === "Space") {
-      items[currentIndex()].click();
-
-    } else if (e.key === "Home") {
-      items[0].focus();
-
-    } else if (e.key === "End") {
-      items[items.length - 1].focus();
-
-    } else if (e.key === "Escape") {
-      toggleMenu(false);
-      button.focus();
+    const currentIndex = () => {
+      const idx = items.indexOf(document.activeElement);
+      if (idx === -1) return 0;
+      return idx;
     }
+
+    menuRoot.addEventListener("keydown", e => {
+      if (e.key === "ArrowUp") {
+        items[currentIndex() - 1]?.focus();
+
+      } else if (e.key === "ArrowDown") {
+        items[currentIndex() + 1]?.focus();
+
+      } else if (e.key === "Space") {
+        items[currentIndex()].click();
+
+      } else if (e.key === "Home") {
+        items[0].focus();
+
+      } else if (e.key === "End") {
+        items[items.length - 1].focus();
+
+      } else if (e.key === "Escape") {
+        toggleMenu(false);
+        button.focus();
+
+      } else if (e.key === "Tab") {
+        toggleMenu(false);
+      }
+    })
   })
-})
+}
+
+addEventListener("htmx:load", e => overflowMenu(e.target));
+
